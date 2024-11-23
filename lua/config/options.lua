df5393761ed7f6742ca2bf06cf4d608b44afea7b
elvxk <pradiptaramadan9c@gmail.com>
2024-11-23 21:28:00 +0700

    update(diagnostic): add border to floating diagnostic

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,14 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
 vim.opt.shell = "pwsh"
+
+vim.diagnostic.config({
+  virtual_text = false, -- Nonaktifkan virtual text jika menggunakan floating window
+  float = {
+    border = "rounded", -- Gaya border pada floating window
+    source = true, -- Tampilkan sumber pesan diagnostik
+    focusable = false, -- Agar floating window tidak bisa difokuskan
+  },
+  underline = true, -- Menambahkan garis bawah untuk diagnosa kesalahan
+  severity_sort = true, -- Mengurutkan berdasarkan tingkat keparahan
+})
